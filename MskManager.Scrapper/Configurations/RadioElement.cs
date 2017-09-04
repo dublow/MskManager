7dd600a4ab8dce5b6af73d1e7373cac45efa4430
nicodelfour <nd@finexkap.com>
2017-09-04 18:59:54 +0200

    Add configuration to MskManager.Scrapper

---
+++ MskManager.Scrapper/Configurations/RadioElement.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+
+namespace MskManager.Scrapper.Configurations
+{
+    public class RadioElement : ConfigurationElement, IRadioConfiguration
+    {
+        [ConfigurationProperty("name", IsKey = true, IsRequired = true)]
+        public string Name {
+            get => (string) base["name"];
+            set => base["name"] = value;
+        }
+
+        [ConfigurationProperty("uri", IsRequired = true)]
+        public string Uri {
+            get => (string) this["uri"];
+            set => base["uri"] = value;
+        }
+    }
+}
