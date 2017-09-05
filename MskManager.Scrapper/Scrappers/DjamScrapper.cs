f5f1a8a257aa4877907329d4ee5c4866871af8a6
nicodelfour <nd@finexkap.com>
2017-09-05 18:09:21 +0200

    Add scrapping methods

---
+++ MskManager.Scrapper/Scrappers/DjamScrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MskManager.Scrapper.Models;
+
+namespace MskManager.Scrapper.Scrappers
+{
+    public class DjamScrapper : IScrapper
+    {
+        private readonly string _uri;
+
+        public DjamScrapper(string uri)
+        {
+            _uri = uri;
+        }
+
+        public Song Scrap()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MskManager.Scrapper/Scrappers/IScrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MskManager.Scrapper.Models;
+
+namespace MskManager.Scrapper.Scrappers
+{
+    public interface IScrapper
+    {
+        Song Scrap();
+    }
+}
