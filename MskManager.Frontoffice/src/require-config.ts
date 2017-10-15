6dca2ed6ef20fa47e9541f58975efb2a0923a1ca
nicodelfour <nd@finexkap.com>
2017-10-15 09:31:18 +0200

    add typescript

---
+++ MskManager.Frontoffice/src/require-config.ts
@@ -0,0 +1,7 @@
+﻿declare var require: any;
+require.config({
+    paths: {
+        "requirejs": "node_modules/requirejs/bin/r",
+        "knockout": "node_modules/knockout/build/output/knockout-latest",
+    }
+});
